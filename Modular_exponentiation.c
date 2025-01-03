84fddc75439f206cd8668b3dae956af62d52733e
yugansh soni <yuganshsoni8@gmail.com>
2025-01-03 13:31:25 +0530

    resolving naming convention and other problems

--- Modular_exponentiation.c
+++ Modular_exponentiation.c
@@ -3,24 +3,33 @@
 
 //we can solve it using thte concept of binary exponentiation.
 //implementing divide and conquer approach to get our result and using multiplication property of mudular with recursion.
-int solve(int base, int power, int M){
+
+int Modular_Exponentiation(int base, int power, int mod_value){
     if(power==0) return 1;
 
-    int sol = (solve(base, power/2, M)%M);
+    int sol = (Modular_Exponentiation(base, power/2, mod_value)%mod_value);
 
     if(power%2==0){
-        return (sol * sol)%M;
+        return (sol * sol)%mod_value;
     }
-    return (((sol * sol)%M)*base)%M;
+    return (((sol * sol)%mod_value)*base)%mod_value;
 
 }
 
 int main(){
 
     int base,power,mod_value;
-    scanf("%d%d%d", &base, &power, &mod_value);
+    printf("Please enter the base : ");
+    scanf("%d", &base);
 
-    printf("answer is : %d", solve(base, power, mod_value));
+    printf("Please enter the power : ");
+    scanf("%d", &power);
+    
+    printf("Please enter the modular value : ");
+    scanf("%d", &mod_value);
+
+    printf("answer is : %d\n", Modular_Exponentiation(base, power, mod_value));
 
     return 0;
+
 }
